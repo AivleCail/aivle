e205d5bc185210a9eba194cb3560adc4cffab1b1
junwoo <studywnsdn0316g@gmail.com>
2023-06-14 11:24:06 +0900

    app.js 오류 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import {Routes, Route} from "react-router-dom"
 import Login from './pages/login/Login';
 import Signup from './pages/login/Signup';
+/* import Intro from './pages/main/intro';
+import VOC from './pages/main/voc';
+import Notice from './pages/main/notice';*/
+
 
 function App() {
   return (
