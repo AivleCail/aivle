3bb7b7e4fd7142aaabdc528bb5c7bb786a813609
Taeho <ice_98@naver.com>
2023-06-14 21:16:13 +0900

    #25 - voc managerId에 맞게 조회가능

---
+++ backend/src/main/java/com/example/backend/voc/dto/VocPageResponseDto.java
@@ -0,0 +1,31 @@
+package com.example.backend.voc.dto;
+
+import com.example.backend.voc.entity.Voc;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Builder
+public class VocPageResponseDto {
+    private Long vocId;
+    private String customerName;
+    private String customerAddress;
+    private String customerPhone;
+    private String type;
+    private String receptionDate;
+    private String checkStatus;
+
+    public static VocPageResponseDto of(Voc voc) {
+        return VocPageResponseDto.builder()
+                .vocId(voc.getId())
+                .customerName(voc.getCustomerName())
+                .customerAddress(voc.getCustomerAddress())
+                .customerPhone(voc.getCustomerPhone())
+                .type(voc.getType())
+                .receptionDate(voc.getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .checkStatus(voc.getStatus())
+                .build();
+    }
+}
