6c49e00bc96631eba7d90633c0d10c01438346af
minkycho0412 <minkycho0412@gmail.com>
2023-06-16 17:27:13 +0900

    REFACTOR: 불필요한 파일 삭제

--- model/model/settings.py
+++ model/model/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stt',
-    'rest_framework',
     'external',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
