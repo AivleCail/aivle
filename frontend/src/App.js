819100e4367193d77b653781c025ea57bcd4ae4f
minkycho0412 <minkycho0412@gmail.com>
2023-06-17 16:31:15 +0900

    REFACTOR: app root 정리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import VOC from './pages/main/voc';
 import Worker from './pages/main/worker'
 import Article from './pages/main/article/articlelist'
 import CreateArticle from './pages/main/article/createarticle'
+import './App.css'
 /* import Intro from './pages/main/intro';
 import VOC from './pages/main/voc';
 import Notice from './pages/main/notice';*/
@@ -14,7 +15,7 @@ import Notice from './pages/main/notice';*/
 
 function App() {
   return (
-    <div classNames="App">
+    <div className="App">
      <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
