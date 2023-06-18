bab52ff70abdfcd08fdeb2e3c30d284e61dfbdbf
minkycho0412 <minkycho0412@gmail.com>
2023-06-18 22:47:46 +0900

    FIX: 리액트 전송 타입 변경 후 장고 연동 확인 및 스프링 전송

--- model/external/urls.py
+++ model/external/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'external'
 
 urlpatterns = [
-    path('', views.ex_index, name='index'),
-    path('result', views.ex_result, name='result'),
     path('api', views.api, name='api')
 ]
