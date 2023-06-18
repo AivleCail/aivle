629c137caf976db5c8c136e13ea544e28e894c55
minkycho0412 <minkycho0412@gmail.com>
2023-06-18 17:03:18 +0900

    FEAT: 사외공사자 css 추가

---
+++ frontend/src/pages/components/list/UnfoldedListBody.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UnfoldedListBody = () => {
+
+    return (
+        <div className='li-body'>
+            <p>업체명: 한국가스공사</p>
+            <p>공사주소: 부산광역시 강서구 명지동 명지오션시티11로 22</p>
+            <p>접수 시간: 2023.06.13 09:00</p>
+            <p>공사 시작 시간: 2023.06.15 13:00</p>
+            <p>공사 종료 시간: 2023.06.15 18:00</p>
+            <p>공사 상세 내용: 가스 점검 및 수리</p>
+        </div>
+    )
+}
+
+export default UnfoldedListBody;
