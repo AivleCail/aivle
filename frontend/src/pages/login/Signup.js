9747f190baae1ee28ae380525ced6df62317b6e5
ssh10421 <egoing@gmail.com>
2023-06-20 17:45:27 +0900

    회원가입 수정, 에러 메세지 추가

--- frontend/src/pages/login/Signup.js
+++ frontend/src/pages/login/Signup.js
@@ -1,49 +1,63 @@
 import React, { useState } from 'react';
 import './Signup.css';
 import axios from 'axios';
-import { Link,useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
-  const [role, setRole] = useState('');
+  const [role, setRole] = useState('manager');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+  const [errorAlert, setErrorAlert] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSignup = () => {
     if (!email || !password || !confirmPassword || !name || !role) {
-      alert('모든 필드를 입력해주세요.');
+      setErrorAlert('');
+      setSubmitted(true);
       return;
     }
 
     if (password !== confirmPassword) {
-      alert('비밀번호가 일치하지 않습니다.');
+      setConfirmPasswordError(true);
       return;
     }
 
     if (!validateEmail(email)) {
-      alert('유효한 이메일을 입력해주세요.');
+      setEmailError(true);
       return;
     }
 
-    axios
-      .post('http://localhost:8080/auth/signup', {
-        name: name,
-        email: email,
-        password: password,
-        auth: role === 'manager' ? 'ROLE_ADMIN' : 'ROLE_USER',
-      }, { withCredentials: true })
-      .then((response) => {
-        console.log('Well done!');
-        console.log('User profile', response.data.user);
-        console.log('User token', response.data.jwt);
-        navigate('/');
-
+    // 사용자 가입 유형을 확인하는 알림창 표시
+    showUserTypeAlert()
+      .then(() => {
+        axios
+          .post('http://localhost:8080/auth/signup', {
+            name: name,
+            email: email,
+            password: password,
+            auth: role === 'manager' ? 'ROLE_ADMIN' : 'ROLE_USER',
+          }, { withCredentials: true })
+          .then((response) => {
+            console.log('Well done!');
+            console.log('User profile', response.data.user);
+            console.log('User token', response.data.jwt);
+            navigate('/');
+          })
+          .catch((error) => {
+            setErrorAlert('이메일이 중복되었습니다.');
+            console.error(error);
+          });
       })
-      
-      .catch((error) => {
-        alert('이메일이 중복되었습니다.', error.response);
+      .catch(() => {
+        // 사용자가 취소한 경우, 필요한 초기화 작업을 수행할 수 있습니다.
+        setRole('manager');
+        setErrorAlert('');
+        setSubmitted(false);
       });
   };
 
@@ -54,6 +68,22 @@ const Signup = () => {
 
   const handleUserType = (selectedRole) => {
     setRole(selectedRole);
+  };
+
+  const showUserTypeAlert = () => {
+    let userType = '';
+    if (role === 'manager') {
+      userType = '매니저';
+    } else if (role === 'external') {
+      userType = '사외공사자';
+    }
+    return new Promise((resolve, reject) => {
+      if (window.confirm(`회원가입을 ${userType}로 진행하시겠습니까?`)) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
   };
 
   return (
@@ -77,14 +107,21 @@ const Signup = () => {
             </div>
             <div className="signup-text-1">이메일</div>
             <div className="group">
-            <div className="input-wrapper">
-              <input
-                type="text"
-                className="text-input"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
+              <div className="input-wrapper">
+                <input
+                  type="text"
+                  className="text-input"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    setEmailError(false);
+                    setErrorAlert('');
+                    setSubmitted(false);
+                  }}
+                />
+                {submitted && !email && <span className="field-error-message">필드를 입력해주세요.</span>}
+                {emailError && <span className="email-error-message">유효한 이메일을 입력해주세요.</span>}
+              </div>
             </div>
             <div className="signup-text-2">비밀번호</div>
             <div className="overlap-group-wrapper-1">
@@ -93,22 +130,31 @@ const Signup = () => {
                   type="password"
                   className="text-input"
                   value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    setErrorAlert('');
+                    setSubmitted(false);
+                  }}
                 />
+                {submitted && !password && <span className="field-error-message">필드를 입력해주세요.</span>}
               </div>
             </div>
             <div className="signup-text-3">비밀번호 확인</div>
             <div className="overlap-group-wrapper-2">
               <div className="div">
-              <input 
-              type="password"
-              className="text-input"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-
-
-      
+                <input
+                  type="password"
+                  className="text-input"
+                  value={confirmPassword}
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                    setConfirmPasswordError(false);
+                    setErrorAlert('');
+                    setSubmitted(false);
+                  }}
+                />
+                {submitted && !confirmPassword && <span className="field-error-message">필드를 입력해주세요.</span>}
+                {confirmPasswordError && <span className="password-error-message">비밀번호가 일치하지 않습니다.</span>}
               </div>
             </div>
             <div className="signup-text-4">이름</div>
@@ -118,19 +164,19 @@ const Signup = () => {
                   type="text"
                   className="text-input"
                   value={name}
-                  onChange={(e) => setName(e.target.value)}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                    setErrorAlert('');
+                    setSubmitted(false);
+                  }}
                 />
+                {submitted && !name && <span className="field-error-message">필드를 입력해주세요.</span>}
               </div>
             </div>
-
-            
-            
-            
-            
             <div className="group-2">
-            <button className="signup-button" onClick={handleSignup}>
-              다 음
-            </button>
+              <button className="signup-button" onClick={handleSignup}>
+                다 음
+              </button>
             </div>
             <img
               className="img"
@@ -138,12 +184,11 @@ const Signup = () => {
               src={process.env.PUBLIC_URL + 'login_background.png'}
             />
           </div>
-          
           <div className="signup_title">회원가입</div>
         </div>
       </div>
+      {errorAlert && <div className="error-alert">{errorAlert}</div>}
     </div>
-
   );
 };
 
