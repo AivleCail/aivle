70ce6301825643a9d05d024fc355c24d06bc8d85
kyuyoung <jky5457@naver.com>
2023-06-21 20:28:42 +0900

    voc, 사외공사 모달 세부 디자인1

--- frontend/src/pages/components/modal/WorkerContent.js
+++ frontend/src/pages/components/modal/WorkerContent.js
@@ -3,14 +3,40 @@ import './WorkerContent.css'
 
 const WorkerContent = ({ worker }) => {
     return (
-        <div> 
-          <p>회사명 : {worker.companyName}</p>
-          <p>접수내역 : {worker.receiptContent}</p>
-          <p>공사주소 : {worker.externalAddress}</p>
-          <p>공사시작일시 : {worker.externalStartdate}</p>
-          <p>공사종료일시 : {worker.externalEnddate}</p>
-          <p>공사상태 : {worker.externalStatus}</p>
-          <p>접수일시 : {worker.receiptDate}</p>
+        <div className="report-container">
+          <h2>사외공사 {worker.externalId}번 상세 내역</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td className='tdl'>회사명</td>
+                <td>{worker.companyName}</td>
+              </tr>
+              <tr>
+                <td className='tdl'>접수내역</td>
+                <td>{worker.receiptContent}</td>
+              </tr>
+              <tr>
+                <td className='tdl'>공사위치</td>
+                <td>{worker.externalAddress}</td>
+              </tr>
+              <tr>
+                <td className='tdl'>공사시작일시</td>
+                <td>{worker.externalStartdate}</td>
+              </tr>
+              <tr>
+                <td className='tdl'>공사종료일시</td>
+                <td>{worker.externalEnddate}</td>
+              </tr>
+              <tr>
+                <td className='tdl'>진행상태</td>
+                <td>{worker.externalStatus}</td>
+              </tr>
+              <tr>
+                <td className='tdl'>접수일시</td>
+                <td>{worker.receiptDate}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
     );
 };
