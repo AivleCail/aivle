2cdacfd02dc6d5867caa4316ff6956331e53303e
kyuyoung <jky5457@naver.com>
2023-06-22 15:10:47 +0900

    footer 디자인 수정

--- frontend/src/pages/components/footer.js
+++ frontend/src/pages/components/footer.js
@@ -4,11 +4,11 @@ import './footer.css';
 const Footer = () => {
   return (
     <div className="footer">
-      <span className="footer-text">
-        <a class="content-secure" href="http://www.naver.com" target="_blank">개인정보 처리방침      </a>
-        <a class="content-secure2" href="http://www.naver.com" target="_blank">이용약관      </a>
-        copyright© AIVLE 부산/경남 37조
-      </span>
+      <div className="footer-text">
+        <a class="content-secure" href="http://www.naver.com" target="_blank">개인정보 처리방침</a>
+        <a class="content-secure2" href="http://www.naver.com" target="_blank">이용약관</a>
+        <a class="copyright">copyright© AIVLE 부산/경남 37조</a>
+      </div>
     </div>
   );
 };
