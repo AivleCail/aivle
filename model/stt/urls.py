558ca8d3dc024d1d0533c8aff2e44f366d70c1ed
Taeho <ice_98@naver.com>
2023-06-22 16:58:15 +0900

    접수내용을 사용자가 볼수있게 한다.

--- model/stt/urls.py
+++ model/stt/urls.py
@@ -6,4 +6,5 @@ app_name = 'stt'
 urlpatterns = [
     path('voc_api', views.voc_api, name='voc_api'),
     path('external_api', views.external_api, name='external_api'),
+    path('external_check', views.external_check, name='voc_check'),
 ]
