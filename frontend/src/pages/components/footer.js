d24a0b1ed8359b269b5fd9dc4586644b01fe4239
daye <dayepark0905@gmail.com>
2023-06-23 17:20:57 +0900

    footer 위치 및 모달 추가

--- frontend/src/pages/components/footer.js
+++ frontend/src/pages/components/footer.js
@@ -1,55 +1,48 @@
 import React, { useState } from 'react';
-import Modal from 'react-modal';
+import Modal from '../components/modal/Modal';
 import './footer.css';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '400px',
-    padding: '20px',
-    borderRadius: '8px',
-    backgroundColor: '#fff',
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    zIndex: 1000,
-  },
-  
-};
 
 const Footer = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
+  
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isOpenModal2, setIsOpenModal2] = useState(false);
 
   const openModal = () => {
-    setModalOpen(true);
+    try {
+      setIsOpenModal(true);
+    } catch (error) {
+      console.error('Error fetching voc details:', error);
+    }
   };
 
+  const openModal2 = () => {
+    try {
+      setIsOpenModal2(true);
+    } catch (error) {
+      console.error('Error fetching voc details:', error);
+    }
+  };
+
+  // Modal Close
   const closeModal = () => {
-    setModalOpen(false);
+    setIsOpenModal(false);
+  };
+
+  const closeModal2 = () => {
+    setIsOpenModal2(false);
   };
   return (
     <div className="footer">
       <div className="footer-text">
-      <span className="content-secure" href="#" onClick={openModal}>
-        개인정보 처리방침
-      </span>
-
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="개인정보 처리방침"
-      >
-        <h2>개인정보 처리방침</h2>
-        <p>이곳에 개인정보 처리방침 내용을 추가합니다.</p>
-
-        <button className="close-button" onClick={closeModal}>
-          <img src={process.env.PUBLIC_URL + "xicon1.png"} alt="Close" />
-        </button>
-      </Modal>
-        <a class="content-secure2" href="http://www.naver.com" target="_blank">이용약관</a>
+        <label className='content-secure'><strong onClick={openModal}>개인정보 처리방침</strong></label>
+        {isOpenModal && (
+        <Modal isOpen={isOpenModal} closeModal={closeModal} entity="userInfo"/>
+      )}  
+        <label className='content-secure2'><strong onClick={openModal2}>이용약관</strong></label>
+        {isOpenModal2 && (
+        <Modal isOpen={isOpenModal2} closeModal={closeModal2} entity="userInfo2"/>
+      )}
         <a class="copyright">copyright© AIVLE 부산/경남 37조</a>
       </div>
     </div>
---
+++ frontend/src/pages/components/modal/UserYakwan.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import './UserYakwan.css'
+
+const UserYakwan = ({userInfo2}) => {
+    return (
+        <div className="report-container2">
+          <h2>이용약관</h2>
+          <table>
+            <tbody>
+              <tr>
+                쉽고 편리한 온라인 서비스를 제공하겠습니다.
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
+    );
+};
+export default UserYakwan;
