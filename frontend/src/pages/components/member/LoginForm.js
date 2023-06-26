87eceb0937c78ab97be07680037e1dfe71f978f5
daye <dayepark0905@gmail.com>
2023-06-26 14:40:46 +0900

    로그인 엔터 구현 및 모달 다시 수정 겹치는건 나중에

--- frontend/src/pages/components/member/LoginForm.js
+++ frontend/src/pages/components/member/LoginForm.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './Member.css';
 
 const LoginForm = ({ activeIndex, handleManagerClick, handleExternalWorkerClick, email, password, setEmail, setPassword, handleLogin, emailErr, passwordErr, err }) => {
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleLogin();
+    }
+  };
+
   return (
     <div className="login-form">
       <ul className='login-ul'>
         <li onClick={handleManagerClick} className={`radio-li ${activeIndex === 0 ? 'login-active' : ''}`}>
           <p className='login-li-text'>매니저</p>
-          </li>
+        </li>
         <li onClick={handleExternalWorkerClick} className={`radio-li ${activeIndex === 1 ? 'login-active' : ''}`}>
           <p className='login-li-text'>사외공사자</p>
         </li>
@@ -15,17 +21,17 @@ const LoginForm = ({ activeIndex, handleManagerClick, handleExternalWorkerClick,
       <div className='login-input-form'>
         <div className='input-border'>
           <img className="icon-input" alt="Element" src={process.env.PUBLIC_URL + '/login_id.png'} />
-          <input type="text" className="text-input" placeholder="이메일 입력" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="text" className="text-input" placeholder="이메일 입력" value={email} onChange={(e) => setEmail(e.target.value)} onKeyPress={handleKeyPress} />
         </div>
         <div className='input-border'>
           <img className="icon-input" alt="Element" src={process.env.PUBLIC_URL + '/login_pw.png'} />
-          <input type="password" className="text-input" placeholder="비밀번호 입력" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="password" className="text-input" placeholder="비밀번호 입력" value={password} onChange={(e) => setPassword(e.target.value)} onKeyPress={handleKeyPress} />
         </div>
       </div>
       <div className='error-group'>
         {emailErr && <span className="log-error-message e-email">이메일을 입력해주세요.</span>}
         {passwordErr && <span className="log-error-message e-pw">비밀번호를 입력해주세요.</span>}
-        {err && ( <div className="log-error-message e-co" dangerouslySetInnerHTML={{ __html: err }} /> )}
+        {err && (<div className="log-error-message e-co" dangerouslySetInnerHTML={{ __html: err }} />)}
       </div>
       <button type="submit" className="submit" onClick={handleLogin}>로그인</button>
     </div>
