97250e537af1a3eaa5fd314b0ac4924e75ef44fa
ssh10421 <egoing@gmail.com>
2023-06-27 01:23:39 +0900

    voc 모달 수정, 추가 수정 필요

--- frontend/src/pages/components/modal/WorkerContent.js
+++ frontend/src/pages/components/modal/WorkerContent.js
@@ -4,36 +4,66 @@ import './WorkerContent.css'
 const WorkerContent = ({ worker }) => {
     return (
         <div className="report-container">
-          <h2>사외공사 {worker.externalId}번 상세 내역</h2>
+          <div>
+            <img className="worker-icon-img" alt="Element" src={process.env.PUBLIC_URL + '/worker-icon.png'} />
+            <p>
+              <h3 className='worker-id'>사외공사 {worker.externalId}번</h3>
+              <h1 className='worker-name'>{worker.companyName}</h1>
+            </p>
+            <p className="worker-status">
+              {worker.externalStatus === '공사중' || worker.externalStatus === '공사 중' ? (
+                <div>
+                  <span style={{ background: 'rgba(255, 193, 193, 0.5)' }}>공사중</span>
+                </div>
+              ) : worker.externalStatus === '공사예정' || worker.externalStatus === '공사 예정' ? (
+                <div>
+                  <span style={{ background: 'rgba(255, 255, 210, 0.8)' }}>공사예정</span>
+                </div>
+              ) : worker.externalStatus === '공사완료' || worker.externalStatus === '공사 완료' ? (
+                <div>
+                  <span style={{ background: 'rgba(200, 255, 200, 0.6)' }}>공사완료</span>
+                </div>
+              ) : null}
+            </p>
+          </div>
           <table>
             <tbody>
               <tr>
-                <td className='tdl'>회사명</td>
-                <td>{worker.companyName}</td>
+                <tr className='trl'>
+                  <img className="worker-number-icon" alt="Element" src={process.env.PUBLIC_URL + '/voc-number-icon.png'} />
+                    접수내역
+                </tr>
+                <tr className="trl-worker">{worker.receiptContent}</tr>
               </tr>
               <tr>
-                <td className='tdl'>접수내역</td>
-                <td>{worker.receiptContent}</td>
+                <tr className='trl'>
+                <img className="worker-number-icon" alt="Element" src={process.env.PUBLIC_URL + '/voc-number-icon.png'} />
+                  공사위치
+                </tr>
+                <tr className="trl-worker">{worker.externalAddress}</tr>
               </tr>
               <tr>
-                <td className='tdl'>공사위치</td>
-                <td>{worker.externalAddress}</td>
-              </tr>
+                <td>
+                  <tr className='trl'>
+                  <img className="worker-number-icon" alt="Element" src={process.env.PUBLIC_URL + '/voc-number-icon.png'} />
+                    공사시작일시
+                  </tr>
+                  <tr className="trl-worker">{worker.externalStartrate}</tr>
+                </td>
+                <td>
+                  <tr className='trl'>
+                  <img className="worker-number-icon" alt="Element" src={process.env.PUBLIC_URL + '/voc-number-icon.png'} />
+                    공사종료일시
+                  </tr>
+                  <tr className="trl-worker">{worker.externalEnddate}</tr>
+                </td>
+              </tr>         
               <tr>
-                <td className='tdl'>공사시작일시</td>
-                <td>{worker.externalStartdate}</td>
-              </tr>
-              <tr>
-                <td className='tdl'>공사종료일시</td>
-                <td>{worker.externalEnddate}</td>
-              </tr>
-              <tr>
-                <td className='tdl'>진행상태</td>
-                <td>{worker.externalStatus}</td>
-              </tr>
-              <tr>
-                <td className='tdl'>접수일시</td>
-                <td>{worker.receiptDate}</td>
+                <tr className='trl'>
+                <img className="worker-number-icon" alt="Element" src={process.env.PUBLIC_URL + '/voc-number-icon.png'} />
+                  접수일시
+                </tr>
+                <tr className="trl-worker">{worker.receiptrate}</tr>
               </tr>
             </tbody>
           </table>
