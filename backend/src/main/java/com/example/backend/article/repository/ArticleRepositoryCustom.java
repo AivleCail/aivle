cddd32eb1e27c2c5719bb3fd5daa370ddb0b7909
jooseongmin <by065001@gmail.com>
2023-06-28 14:50:50 +0900

    intro수정부분입니다

--- backend/src/main/java/com/example/backend/article/repository/ArticleRepositoryCustom.java
+++ backend/src/main/java/com/example/backend/article/repository/ArticleRepositoryCustom.java
@@ -1,14 +1,17 @@
 package com.example.backend.article.repository;
 
 
+import com.example.backend.article.dto.ArticleIntroResponseDto;
 import com.example.backend.article.dto.ArticlePageResponseDto;
+import com.example.backend.voc.dto.VocAnswerDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 
 
 public interface ArticleRepositoryCustom {
     Page<ArticlePageResponseDto> searchAll(Pageable pageable);
 
-
+    List<ArticleIntroResponseDto> best();
 }
